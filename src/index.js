65e20015f58a8a330df23c02d34c2badee854dcd
mackeee-orange <makino.aaa@gmail.com>
2021-02-15 11:22:45 +0900

    hoge

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
+// add comment
+
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
